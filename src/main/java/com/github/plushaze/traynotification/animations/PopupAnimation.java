2bf7829fde738751e4d85a54744506132b7fed3d
Javier Benítez <javierbentezgarca2@icloud.com>
2024-01-25 12:51:09 +0100

    Change PopupAnimation.java

--- src/main/java/com/github/plushaze/traynotification/animations/PopupAnimation.java
+++ src/main/java/com/github/plushaze/traynotification/animations/PopupAnimation.java
@@ -12,39 +12,39 @@ final class PopupAnimation extends AbstractAnimation {
 
 	@Override
 	protected Timeline setupDismissAnimation() {
-		Timeline tl = new Timeline();
+	 Timeline tl = new Timeline();
 
-		KeyValue kv1 = new KeyValue(stage.yLocationProperty(), stage.getY() + stage.getWidth());
-		KeyFrame kf1 = new KeyFrame(Duration.millis(2000), kv1);
+	 KeyValue kv1 = new KeyValue(stage.yLocationProperty(), stage.getY() + stage.getWidth(), Interpolator.EASE_BOTH);
+	 KeyFrame kf1 = new KeyFrame(Duration.millis(2000), kv1);
 
-		KeyValue kv2 = new KeyValue(stage.opacityProperty(), 0.0);
-		KeyFrame kf2 = new KeyFrame(Duration.millis(2000), kv2);
+	 KeyValue kv2 = new KeyValue(stage.opacityProperty(), 0.0, Interpolator.EASE_BOTH);
+	 KeyFrame kf2 = new KeyFrame(Duration.millis(2000), kv2);
 
-		tl.getKeyFrames().addAll(kf1, kf2);
+	 tl.getKeyFrames().addAll(kf1, kf2);
 
-		tl.setOnFinished(e -> {
-			trayIsShowing = false;
-			stage.close();
-			stage.setLocation(stage.getBottomRight());
-		});
+	 tl.setOnFinished(e -> {
+	  trayIsShowing = false;
+	  stage.close();
+	  stage.setLocation(stage.getBottomRight());
+	 });
 
-		return tl;
+	 return tl;
 	}
 
 	@Override
 	protected Timeline setupShowAnimation() {
 		Timeline tl = new Timeline();
 
-		KeyValue kv1 = new KeyValue(stage.yLocationProperty(), stage.getBottomRight().getY() + stage.getWidth());
+		KeyValue kv1 = new KeyValue(stage.yLocationProperty(), stage.getBottomRight().getY() + stage.getWidth(), Interpolator.EASE_BOTH);
 		KeyFrame kf1 = new KeyFrame(Duration.ZERO, kv1);
 
-		KeyValue kv2 = new KeyValue(stage.yLocationProperty(), stage.getBottomRight().getY());
-		KeyFrame kf2 = new KeyFrame(Duration.millis(1000), kv2);
+		KeyValue kv2 = new KeyValue(stage.yLocationProperty(), stage.getBottomRight().getY(), Interpolator.EASE_BOTH);
+		KeyFrame kf2 = new KeyFrame(Duration.millis(2000), kv2);
 
-		KeyValue kv3 = new KeyValue(stage.opacityProperty(), 0.0);
+		KeyValue kv3 = new KeyValue(stage.opacityProperty(), 0.0, Interpolator.EASE_BOTH);
 		KeyFrame kf3 = new KeyFrame(Duration.ZERO, kv3);
 
-		KeyValue kv4 = new KeyValue(stage.opacityProperty(), 1.0);
+		KeyValue kv4 = new KeyValue(stage.opacityProperty(), 1.0, Interpolator.EASE_BOTH);
 		KeyFrame kf4 = new KeyFrame(Duration.millis(2000), kv4);
 
 		tl.getKeyFrames().addAll(kf1, kf2, kf3, kf4);
